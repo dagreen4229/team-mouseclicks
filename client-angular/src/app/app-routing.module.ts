6774e03acd70d5e7b30c25c1af808febc83d67c1
Bluejay1103 <Jayson_Shoemaker@aol.com>
2020-01-22 20:34:25 -0800

    added links to client/provider log ins

--- client-angular/src/app/app-routing.module.ts
+++ client-angular/src/app/app-routing.module.ts
@@ -3,14 +3,24 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PLoginComponent } from './modules/provider-ui/login/login.component';
+//import { HomeComponent } from './modules/home/home.component';
+import { CloginComponent } from './modules/client-ui/clogin/clogin.component';
 
 
 const routes: Routes = [
 
+  /*{
+    path: '',
+    component: HomeComponent
+  },*/
   {
     path: 'plogin',
     component: PLoginComponent
   },
+  {
+    path: 'clogin',
+    component: CloginComponent
+  }
 
 ];
 
--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { PLoginComponent } from './modules/provider-ui/login/login.component';
+import { CloginComponent } from './modules/client-ui/clogin/clogin.component';
 import { AuthenticationComponent } from './Shared/services/authentication/authentication.component';
+import { HomeComponent } from './modules/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, PLoginComponent, AuthenticationComponent],
+  declarations: [AppComponent, PLoginComponent, AuthenticationComponent, HomeComponent, CloginComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
