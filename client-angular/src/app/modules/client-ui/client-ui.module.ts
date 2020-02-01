40b5dd0fd8848b80eebbfbf3e84660881ad5cb93
Bluejay1103 <Jayson_Shoemaker@aol.com>
2020-01-31 20:13:18 -0800

    added client-ui components

--- client-angular/src/app/modules/client-ui/client-ui.module.ts
+++ client-angular/src/app/modules/client-ui/client-ui.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CloginComponent } from './clogin/clogin.component';
+import { CdashboardComponent } from './cdashboard/cdashboard.component';
+import { CregisterComponent } from './cregister/cregister.component';
+import { CsetupAccountComponent } from './cregister/csetup-account/csetup-account.component';
+import { CalendarComponent } from './cdashboard/calendar/calendar.component';
 
 @NgModule({
 
-  declarations: [CloginComponent],
+  declarations: [CloginComponent, CdashboardComponent, CregisterComponent, CsetupAccountComponent, CalendarComponent],
 
   imports: [
     CommonModule
