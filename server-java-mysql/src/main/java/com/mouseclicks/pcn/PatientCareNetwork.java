7d437ddca95d9dce5501e6171fbf9e5ca04b6b0e
Dave <dagreen4229@gmail.com>
2020-02-02 15:44:21 -0700

    fixed file conflict errors

--- server-java-mysql/src/main/java/com/mouseclicks/pcn/PatientCareNetwork.java
+++ server-java-mysql/src/main/java/com/mouseclicks/pcn/PatientCareNetwork.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD:server-java-mysql/src/main/java/com/mouseclicks/pcn/PatientApplication.java
-package com.mouseclicks.pcn;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@SpringBootApplication
-public class PatientApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PcnApplication.class, args);
-	}
-	
-	@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000","http://localhost:4200");
-            }
-        };
-    }
-
-}
-=======
 package com.mouseclicks.pcn;
 
 import org.springframework.boot.SpringApplication;
@@ -52,4 +24,4 @@ public class PatientCareNetwork {
     }
 
 }
->>>>>>> dev-david:server-java-mysql/src/main/java/com/mouseclicks/pcn/PatientCareNetwork.java
+
