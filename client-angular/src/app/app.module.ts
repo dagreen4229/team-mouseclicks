001fbc630cf8a06b604ca231e3662071fa784197
Bluejay1103 <Jayson_Shoemaker@aol.com>
2020-02-01 19:10:53 -0800

    organized app.module.ts

--- client-angular/src/app/app.module.ts
+++ client-angular/src/app/app.module.ts
@@ -3,34 +3,38 @@ import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTabsModule } from '@angular/material/tabs';
 
 // used to create fake backend
 import { fakeBackendProvider } from './fakeBackend';
 import { JwtInterceptor, ErrorInterceptor } from './fakeBackend';
 
+// Services
+import { FilterPipe } from './Shared/services';
+
+// Client side components
+import { CloginComponent } from './modules/client-ui/clogin/clogin.component';
+import { CdashboardComponent } from './modules/client-ui/cdashboard/cdashboard.component';
+import { CalendarComponent } from './modules/client-ui/cdashboard/calendar/calendar.component';
+import { CregisterComponent } from './modules/client-ui/cregister/cregister.component';
+import { CsetupAccountComponent } from './modules/client-ui/cregister/csetup-account/csetup-account.component';
+
+// Provider side components
 import { AppComponent } from "./app.component";
 import { PLoginComponent } from './modules/provider-ui/login/login.component';
-import { CloginComponent } from './modules/client-ui/clogin/clogin.component';
 import { HomeComponent } from './modules/home/home.component';
 import { DashboardComponent } from './modules/provider-ui/dashboard/dashboard.component';
-import { CdashboardComponent } from './modules/client-ui/cdashboard/cdashboard.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatTabsModule } from '@angular/material/tabs';
-//import { FilterPipe } from './Shared/services/filter.pipe';
-//import { ProviderUIModule } from './modules/provider-ui/provider-ui.module';
 import { PaccountComponent } from './modules/provider-ui/paccount/paccount.component';
 import { ViewCalendarComponent } from './modules/provider-ui/dashboard/view-calendar/view-calendar.component';
-import { CalendarComponent } from './modules/client-ui/cdashboard/calendar/calendar.component';
 import { SearchRecordsComponent } from './modules/provider-ui/dashboard/search-records/search-records.component';
 import { DisplayClientComponent } from './modules/provider-ui/dashboard/display-client/display-client.component';
 import { SearchClientsComponent } from './modules/provider-ui/dashboard/search-clients/search-clients.component';
 import { RegisterComponent } from './modules/provider-ui/register/register.component';
-import { CregisterComponent } from './modules/client-ui/cregister/cregister.component';
 import { SetupAccountComponent } from './modules/provider-ui/register/setup-account/setup-account.component';
-import { CsetupAccountComponent } from './modules/client-ui/cregister/csetup-account/csetup-account.component';
 import { ClientDetailComponent } from './modules/provider-ui/dashboard/client-detail/client-detail.component';
-import { FilterPipe } from './Shared/services';
+
 
 
 
