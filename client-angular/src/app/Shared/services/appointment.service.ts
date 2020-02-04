3585622fa5fc1b6c6e038d38604136cd0a64808c
Bluejay1103 <Jayson_Shoemaker@aol.com>
2020-02-03 16:06:27 -0800

    added appointment table

---
+++ client-angular/src/app/Shared/services/appointment.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Appointment } from "src/app/models/appointment"
+import { Observable, of } from "rxjs";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppointmentService {
+  url: string = "http://localhost:3000/appointments"
+
+  getAppointments(): Observable<Appointment[]> {
+    return this.http.get<Appointment[]>(this.url);
+  }
+
+
+  constructor(private http: HttpClient) { }
+}
