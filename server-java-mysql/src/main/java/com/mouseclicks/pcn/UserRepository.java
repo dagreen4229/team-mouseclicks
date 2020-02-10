5e5878ab55e337c2f8c60341f6756eccba2f3360
Ja'Spring <jsd199523@gmail.com>
2020-02-10 11:55:55 -0600

    updated info

--- server-java-mysql/src/main/java/com/mouseclicks/pcn/UserRepository.java
+++ server-java-mysql/src/main/java/com/mouseclicks/pcn/UserRepository.java
@@ -1,5 +1,10 @@
 package com.mouseclicks.pcn;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.User;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+User findByUsername(String username);
+	
 }
