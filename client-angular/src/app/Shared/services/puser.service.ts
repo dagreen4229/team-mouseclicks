590c226bc53a6baaf49922db43201209d282aa3d
David <dave@dagreen4229.me>
2020-02-14 11:04:18 -0700

    still adjusting URLS

--- client-angular/src/app/Shared/services/puser.service.ts
+++ client-angular/src/app/Shared/services/puser.service.ts
@@ -11,23 +11,23 @@ export class UserService {
 
 
     getAll() {
-        return this.http.get<Puser[]>(`${environment.apiUrl}/users`);
+        return this.http.get<Puser[]>(`${environment.apiUrl}/puser`);
     }
 
     getById(id: number) {
-        return this.http.get(`${environment.apiUrl}/users/${id}`);
+        return this.http.get(`${environment.apiUrl}/puser/${id}`);
     }
 
     register(user: Puser) {
-        return this.http.post(`${environment.apiUrl}/users/register`, user);
+        return this.http.post(`${environment.apiUrl}/puser/register`, user);
     }
 
     update(user: Puser) {
-        return this.http.put(`${environment.apiUrl}/users/${user.User_ID}`, user);
+        return this.http.put(`${environment.apiUrl}/puser/${user.User_ID}`, user);
     }
 
     delete(id: number) {
-        return this.http.delete(`${environment.apiUrl}/users/${id}`);
+        return this.http.delete(`${environment.apiUrl}/puser/${id}`);
     }
 }
 */
