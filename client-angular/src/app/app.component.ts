a3960aa9fd17fb4168632781c0fc1d188ddd119f
David <dave@dagreen4229.me>
2020-02-14 11:06:35 -0700

    URLS

--- client-angular/src/app/app.component.ts
+++ client-angular/src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'PCN';
 
-  apiUrl: string = "http://localhost:3000";
+  apiUrl: string = "http://localhost:8080";
 }
