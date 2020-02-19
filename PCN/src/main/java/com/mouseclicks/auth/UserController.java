173b749447b99bd407f42cb63adbb75c5894f778
David <dave@dagreen4229.me>
2020-02-19 11:02:04 -0700

    Added getmapping with a path variable for id

--- PCN/src/main/java/com/mouseclicks/auth/UserController.java
+++ PCN/src/main/java/com/mouseclicks/auth/UserController.java
@@ -1,17 +1,26 @@
 package com.mouseclicks.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/register")
 public class UserController {
 
   @Autowired
   private MySQLUserDetailsService userService;
 
-  @PostMapping("/user")
+  @PostMapping("/regiter/user")
   public void register(@RequestBody User newUser) {
     userService.Save(newUser);
+  }
+  @GetMapping("/pdashboard/{id}")
+  public ResponseEntity<String> withUserId(@PathVariable String id) {
+	  return ResponseEntity.ok(String.format(id));
+	  
   }
 }
